684708fef261e2de1906cdcd7dcade69a4435e07
Matteo Bonacini <26163503+P2-718na@users.noreply.github.com>
2025-01-25 15:32:17 +0100

    Add some ppu registers

--- src/Graphics/graphics.cpp
+++ src/Graphics/graphics.cpp
@@ -1,14 +1,78 @@
+#include <cassert>
+#include <bitset>
 #include "graphics.hpp"
 
 #include "memory.hpp"
 
 namespace gb {
 
-Graphics::Graphics() = default;
+bool Graphics::LCDC(LCDCBit flag) const {
+  const std::bitset<8> reg = ram_->read(LCDCAddress);
+  return reg[flag];
+}
+
+void Graphics::LCDC(LCDCBit flag, bool value) {
+  std::bitset<8> reg = ram_->read(LCDCAddress);
+  reg[flag] = value;
+  ram_->write(LCDCAddress, reg.to_ulong()); // Todo test
+}
+
+bool Graphics::STAT(STATBit flag) const {
+  const std::bitset<8> reg = ram_->read(STATAddress);
+  return reg[flag];
+}
+
+void Graphics::STAT(STATBit flag, bool value) {
+  std::bitset<8> reg = ram_->read(STATAddress);
+  reg[flag] = value;
+  ram_->write(STATAddress, reg.to_ulong()); // Todo test
+}
+
+
+void Graphics::setPPUMode(PPUMode mode) {
+  assert(mode >= 0 && mode <= 3);
+
+  switch (mode) {
+    case HBlank:
+      STAT(PPU_Mode_msb, 0);
+      STAT(PPU_Mode_lsb, 0);
+    break;
+    case VBlank:
+      STAT(PPU_Mode_msb, 0);
+      STAT(PPU_Mode_lsb, 1);
+    break;
+    case OAMScan:
+      STAT(PPU_Mode_msb, 1);
+      STAT(PPU_Mode_lsb, 0);
+    break;
+    case Drawing:
+      STAT(PPU_Mode_msb, 1);
+      STAT(PPU_Mode_lsb, 1);
+    break;
+  }
+}
+
+
+Graphics::Graphics() {
+  STAT(STAT_Unused_Bit, 1);
+};
 
 
 void Graphics::connectMemory(Memory* ram) {
   ram_ = ram;
 }
+
+PPUMode Graphics::getPPUMode() const {
+  return STAT(PPU_Mode_msb) << 1 | STAT(PPU_Mode_lsb);
+}
+
+word Graphics::WY() const {
+  return ram_->read(WYAddress);
+}
+
+word Graphics::WX() const {
+  return ram_->read(WXAddress);
+}
+
 
 } // namespace gb
--- src/Graphics/graphics.hpp
+++ src/Graphics/graphics.hpp
@@ -7,9 +7,85 @@ namespace gb {
 
 class Graphics {
 
-public:
+  // Graphics data encoded in the 2BPP format (explained above)
+  // is stored in VRAM at addresses $8000-$97FF and is
+  // usually referred to by so-called “Tile Numbers”.
+  // As each tile takes up 16 bytes of memory, a
+  // “Tile Number” is essentially just an index of a
+  // 16-byte-block within this section of VRAM.
+
+  // In order to set which tiles should be displayed in the Background / Window
+  // grids, background maps are used. The VRAM sections $9800-$9BFF and
+  // $9C00-$9FFF each contain one of these background maps.
+  // A background map consists of 32x32 bytes representing tile numbers organized
+  // row by row.
+  // This means that the first byte in a background map is the Tile Number of
+  // the Tile at the very top left
+  // The byte after is the Tile Number of the Tile to the right of it and so
+  // on. The 33rd byte would represent the Tile Number of the leftmost tile in the
+  // second tile row.
+
+  // The OAM (standing for “Object Attribute Memory”)
+  // section of memory ranges from $FE00-$FE9F and contains
+  // data used to display Sprites (also known as “Objects”)
+  // on screen. Each sprite takes up 4 bytes in this section
+  // of memory, allowing for a total of 40 sprites to be displayed
+  // at any given time. Each entry is structured as follows:
+
+  const dword LCDCAddress = 0xFF40;
+  const dword STATAddress = 0xFF41;
+  const dword SCYAddress  = 0xFF42;
+  const dword SCXAddress  = 0xFF43;
+  const dword LYAddress   = 0xFF44;
+  const dword LYACddress  = 0xFF45;
+  const dword WYAddress   = 0xFF4A;
+  const dword WXAddress   = 0xFF4B;
+
   //  Todo same as for processor.hpp
   Memory* ram_;
+
+  typedef enum {
+    LCD_Display_Enable      = 7,
+    Window_Tile_Map_Select  = 6,
+    Window_Display_Enable   = 5,
+    Tile_Data_Select        = 4,
+    BG_Tile_Map_Select      = 3,
+    Sprite_Size             = 2,
+    Sprite_Enable           = 1,
+    BG_Window_Enable        = 0,
+  } LCDCBit;
+
+  typedef enum {
+    STAT_Unused_Bit         = 7,
+    Mode_3_Interrupt_Enable = 6,
+    Mode_2_Interrupt_Enable = 5,
+    Mode_1_Interrupt_Enable = 4,
+    Mode_0_Interrupt_Enable = 3,
+    Coincidence_Flag        = 2,
+    PPU_Mode_msb            = 1,
+    PPU_Mode_lsb            = 0, //Todo pandocs does not actually say what is msb and what lsb, check fux
+
+  } STATBit;
+
+  typedef enum {
+    HBlank  = 0,
+    VBlank  = 1,
+    OAMScan = 2,
+    Drawing = 3
+  } PPUMode;
+
+  // Registers
+  // LCD Control Register (LCDC : $FF40)
+  bool LCDC(LCDCBit flag) const;
+  void LCDC(LCDCBit flag, bool value);
+
+  // LCD Status Register (STAT : $FF41)
+  bool STAT(STATBit flag) const;
+  void STAT(STATBit flag, bool value);
+
+  void setPPUMode(PPUMode mode);
+
+ public:
 
   // Constructor ///////////////////////////////////////////////////////////////
   Graphics();
@@ -18,6 +94,11 @@ public:
   //  this class has a reference to gameboy and that it can call ram from that.
   void connectMemory(Memory* ram);
   //////////////////////////////////////////////////////////////////////////////
+
+  PPUMode getPPUMode() const;
+
+  word WY() const;
+  word WX() const;
 };
 
 } // namespace gb
