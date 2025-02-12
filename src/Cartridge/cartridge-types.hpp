597f100d454342e89ddbaae4c284f1e01d966515
Matteo Bonacini <26163503+P2-718na@users.noreply.github.com>
2025-02-12 19:12:39 +0100

    Add mbc0, mbc1

---
+++ src/Cartridge/cartridge-types.hpp
@@ -0,0 +1,8 @@
+#ifndef CARTRIDGE_TYPES_H
+#define CARTRIDGE_TYPES_H
+
+#include "controllers/mbc0.hpp"
+#include "controllers/mbc1.hpp"
+#include "controllers/mbc3.hpp"
+
+#endif  // CARTRIDGE_TYPES_H
