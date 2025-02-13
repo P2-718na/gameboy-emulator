263a13fe1dac03f3362da204a72bb511017b13df
Matteo Bonacini <26163503+P2-718na@users.noreply.github.com>
2025-02-13 11:59:02 +0100

    Add missing includes

--- tests/sample.test.cpp
+++ tests/sample.test.cpp
@@ -1,5 +1,9 @@
+// Doctest requires this define to be specified only once.
+// Probably this is the best place
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 #include "doctest.h"
 
-TEST_CASE("Sample test2") {
-  CHECK(true);
+TEST_CASE("Check that doctest is working") {
+  REQUIRE(true);
 }
